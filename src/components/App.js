0f339d3bb01fa6533b6986d9cc59f1788fccdc6a
GideonRotic1 <rotichgid38@gmail.com>
2024-02-07 15:40:51 +0300

    did react hooks and state events mini project

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,35 @@ import TaskList from "./TaskList";
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
+import React, { useState } from "react";
+import CategoryFilter from "./CategoryFilter";
+import NewTaskForm from "./NewTaskForm";
+import TaskList from "./TaskList";
+
+import { CATEGORIES, TASKS } from "../data";
+console.log("Here's the data you're working with");
+console.log({ CATEGORIES, TASKS });
+
+function App() {
+  const [selectedCategory, setSelectedCategory]  = useState("All")
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category)
+  }
+  return (
+    <div className="App">
+      <h2>My tasks</h2>
+      <CategoryFilter categories={CATEGORIES}
+                      selectedCategory={selectedCategory}
+                      onChangeSelectedCategory={handleCategoryChange}
+      />
+      <NewTaskForm />
+      <TaskList tasks={TASKS} selectedCategory={selectedCategory}/>
+    </div>
+  );
+}
+
+export default App;
 
 function App() {
   return (
